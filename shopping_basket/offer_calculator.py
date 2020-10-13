61f443e11321a117bb47460089c4fc207dacfdf1
sportbilly <vtzitzilonis@gmail.com>
2020-10-13 23:00:38 +0100

    Added the functionality of discount, buy M get N free, N for the price of M

--- shopping_basket/offer_calculator.py
+++ shopping_basket/offer_calculator.py
@@ -1,6 +1,85 @@
+from collections import Counter
+
+
 class Offer_Calculator:
     def __init__(self):
         self._discount = 0
         self._offers = {}
         self._basket = {}
         self._catalogue = {}
+        self._item_occurrences = {}
+        self._item_offers = {}
+
+    def get_overall_discount(self):
+        return round(self._discount, 2)
+
+    # Extract the info for the offers and call the relevant funtions
+    def calculate_offers(self, offers, basket, catalogue):
+        self._basket = basket
+        self._catalogue = catalogue
+        self._offers = offers
+        self._basket = Counter(
+            basket
+        )  # Covert Basket to dictionary with counts of each item
+
+        for key in self._basket:
+            # Find if the items in the basket have any offers call the relevant function
+            if key in self._offers:
+                if "buy M get N free" in self._offers[key]:
+                    if self._basket[key] >= self._offers[key]["buy M get N free"][0]:
+                        self.calculate_get_free(
+                            key,
+                            self._basket[key],
+                            self._offers[key]["buy M get N free"],
+                        )
+                        return
+
+                if "N for the price of M" in self._offers[key]:
+                    if (
+                        self._basket[key]
+                        >= self._offers[key]["N for the price of M"][0]
+                    ):
+                        self.calculate_get_for_the_price(
+                            key,
+                            self._basket[key],
+                            self._offers[key]["N for the price of M"],
+                        )
+                        return
+
+                if "discount" in self._offers[key]:
+                    self.calculate_discount(
+                        key, self._basket[key], self._offers[key]["discount"][0]
+                    )
+
+    def calculate_discount(self, item, num_items, discount):
+        price = self._catalogue[item]
+        discount_price = (price * num_items) * (discount / 100)
+        self._discount += discount_price
+
+    def calculate_get_free(self, item, num_items, get_free):
+        price = self._catalogue[item]
+        free_items = get_free[1] * (
+            num_items // get_free[0]
+        )  # Calculate the free items
+        discount = price * free_items  # Discount for free times
+        extra_item = num_items % get_free[0]
+        if extra_item > 0:
+            if "discount" in self._offers[item]:
+                self.calculate_discount(
+                    item, extra_item, self._offers[item]["discount"][0]
+                )
+        self._discount += discount
+
+    def calculate_get_for_the_price(self, item, num_items, for_the_price):
+        price = self._catalogue[item]
+        items_to_pay = for_the_price[1] * (
+            num_items // for_the_price[0]
+        )  # Calculate the number of items to pay
+        extra_item = num_items % for_the_price[0]
+        if extra_item > 0:
+            if "discount" in self._offers[item]:
+                self.calculate_discount(
+                    item, extra_item, self._offers[item]["discount"][0]
+                )
+        discount = price * num_items - price * (items_to_pay + (extra_item))
+        self._discount += discount
--- shopping_basket/shopping_basket_tests/test_offers.py
+++ shopping_basket/shopping_basket_tests/test_offers.py
@@ -14,15 +14,70 @@ catalogue = {
     "Edam": 1.20,
     "Cheddar": 1.30,
     "Gouda": 1.00,
+    "Yogurt": 3.00,
 }
 
-offers = {
-    "Baked Beans": ["buy 2 get 1 free", "10% discount"],
-    "Shampoo (Large)": ["25% discount"],
-    "Yogurt": ["buy 2 get 1 free"],
-    "Shampoo (Medium)": ["3 for the price of two"],
+offers_dict = {
+    "Baked Beans": {"discount": [15], "buy M get N free": [2, 1]},
+    "Shampoo (Large)": {"discount": [20]},
+    "Yogurt": {"buy M get N free": [2, 1], "discount": [20]},
+    "Shampoo (Medium)": {"N for the price of M": [3, 2]},
 }
+
 
 class Test_offers(unittest.TestCase):
-    def test_discounts(self):  # Check the basket total before discounts
-    return None
+    def test_discounts(self):  #
+        basket = ["Shampoo (Large)", "Shampoo (Large)"]
+        offer = offer_calculator.Offer_Calculator()
+        offer.calculate_offers(offers_dict, basket, catalogue)
+        discount = offer.get_overall_discount()
+        self.assertEqual(discount, 1.40)
+
+    def test_Multidiscounts(self):  #
+        basket = ["Shampoo (Large)", "Shampoo (Large)", "Baked Beans", "Yogurt"]
+        offer = offer_calculator.Offer_Calculator()
+        offer.calculate_offers(offers_dict, basket, catalogue)
+        discount = offer.get_overall_discount()
+        self.assertEqual(discount, 2.15)
+
+    def test_multifree(self):
+        basket = ["Yogurt", "Yogurt", "Yogurt"]
+        offer = offer_calculator.Offer_Calculator()
+        offer.calculate_offers(offers_dict, basket, catalogue)
+        discount = offer.get_overall_discount()
+        self.assertEqual(discount, 3.60)
+
+    def test_combinations_discount__free(self):
+
+        basket = [
+            "Shampoo (Large)",
+            "Yogurt",
+            "Yogurt",
+            "Yogurt",
+        ]
+
+        offer = offer_calculator.Offer_Calculator()
+        offer.calculate_offers(offers_dict, basket, catalogue)
+        discount = offer.get_overall_discount()
+        self.assertEqual(discount, 4.30)
+
+    def test_M_for_price_of_N(self):  # Check the basket total before discounts
+
+        basket = [
+            "Shampoo (Medium)",
+            "Shampoo (Medium)",
+            "Shampoo (Medium)",
+            "Shampoo (Medium)",
+            "Shampoo (Medium)",
+            "Shampoo (Medium)",
+            "Shampoo (Medium)",
+            "Shampoo (Medium)",
+        ]
+        offer = offer_calculator.Offer_Calculator()
+        offer.calculate_offers(offers_dict, basket, catalogue)
+        discount = offer.get_overall_discount()
+        self.assertEqual(discount, 5.00)
+
+
+if __name__ == "__main__":
+    unittest.main()
